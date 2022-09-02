a84f62ef243d6a946dab40f6f06c5bdacf43912f
Michel Pereira <michelbecauro@gmail.com>
2022-09-02 00:12:57 -0300

    FEAT: Add ProductManager.ts class

---
+++ MODEL/ProductManager.ts
@@ -0,0 +1,53 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Product} from './Product';
+
+const salveProduct = async (key: string, value: any) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {}
+};
+const removeProduct = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (e) {}
+};
+const getProductsJSON = async () => {
+  try {
+    let keys: Array<string> = [];
+    keys = await AsyncStorage.getAllKeys();
+    return await AsyncStorage.multiGet(keys);
+  } catch (e) {
+    return [];
+  }
+};
+const obterProducts = async () => {
+  try {
+    let objects: Array<Product> = [];
+    let objJSON = await getProductsJSON();
+    if (objJSON != null && objJSON.length > 0) {
+      objJSON.forEach((element: string) => {
+        let product: Product = JSON.parse(element[1]);
+        objects.push(product);
+      });
+    }
+    return objects;
+  } catch (e) {
+    return [];
+  }
+};
+
+class ProductManager {
+  public async remove(chave: number) {
+    removeProduct(chave.toString());
+  }
+  public async add(product: Product) {
+    salveProduct(product.Code.toString(), product);
+  }
+  public async getAll(): Promise<Array<Product>> {
+    let lista: Array<Product> = await obterProducts();
+    return lista;
+  }
+}
+
+export default ProductManager;
