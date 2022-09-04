ac9a4a1ef6dfcd12ff8d205036052a5334897d2a
Michel Pereira <michelbecauro@gmail.com>
2022-09-03 22:08:32 -0300

    CHORE: Remove comments from ProductList

--- VIEW/ProductList.tsx
+++ VIEW/ProductList.tsx
@@ -1,26 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, FlatList} from 'react-native';
-import Product from '../MODEL/Product';
 import ProductManager from '../MODEL/ProductManager';
 import {styles} from './CommonStyles';
-
-// const defaultData = [
-//   {
-//     Code: 1,
-//     Name: 'default - hello',
-//     Quatity: 2,
-//   },
-//   {
-//     Code: 4,
-//     Name: 'default - hi',
-//     Quatity: 3,
-//   },
-//   {
-//     Code: 6,
-//     Name: 'default - take care',
-//     Quatity: 5,
-//   },
-// ];
 
 type ProductType = {
   Code: number;
@@ -37,17 +18,8 @@ const renderItem = ({item}: any) => (
 const ProductComponent = () => {
   const [DATA, setDATA] = useState(Array<ProductType>);
 
-  // var DATA: any = [];
-
   const loadData = async () => {
-    const p1: ProductType = new Product(1, 'tv', 2);
-    const p2: ProductType = new Product(2, 'desk', 4);
-    const p3: ProductType = new Product(3, 'table', 11);
     const pm: ProductManager = new ProductManager();
-
-    await pm.add(p1);
-    await pm.add(p2);
-    await pm.add(p3);
 
     let newData: Array<ProductType> = await pm.getAll();
     setDATA(newData);
